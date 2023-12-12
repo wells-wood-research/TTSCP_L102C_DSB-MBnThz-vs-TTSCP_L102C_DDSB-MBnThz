48bd69b80eeff6a7eb02fbd24b37896321084fc5
sunal1996 <s1995825@ed.ac.uk>
2023-12-12 14:15:04 +0000

    put scripts

---
+++ TTSCP_L102C_DDSB_scripts/TTSCP_L102C_DDSB_2.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env pythn
+# coding: utf-8
+
+# In[ ]:
+
+
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import *
+from sys import stdout
+
+
+platform=Platform.getPlatformByName("CUDA")
+
+TOTAL_SIM_LENGTH = 200*nanoseconds
+TIME_STEP = 2*femtosecond
+TOTAL_STEPS = int(TOTAL_SIM_LENGTH/TIME_STEP)
+LOG_FREQUENCY_STEPS = int(TOTAL_STEPS / 500)
+
+# Load in Amber files (prepared using AmberTools)
+prmtop = AmberPrmtopFile('TTSCP_L102C_DDSB_energyminimized.prmtop')
+inpcrd = AmberInpcrdFile('TTSCP_L102C_DDSB_energyminimized.inpcrd')
+#pdb_file = 'TTSCP_L102C_DDSB_energyminimized.pdb'
+#pdb = PDBFile(pdb_file)
+#k_rmsd = 100.0*kilocalories_per_mole/angstroms**2
+#rmsd_receptor_0 = 0
+#backbone_receptor=list(range(1538, 1588))
+#print(backbone_receptor)
+# Set up simulation "environment"
+system = prmtop.createSystem(nonbondedMethod=PME, nonbondedCutoff=1*nanometer,
+        constraints=HBonds)
+system.addForce(MonteCarloBarostat(1*bar, 298*kelvin))
+#rmsd_cv =RMSDForce(pdb.positions, backbone_receptor) # RMSD biasing/harmonic force to act on the backbone of the 'receptor' chains
+#rmsd_receptor = CustomCVForce('0.5*k_rmsd*(rmsd-rmsd_target)^2')
+#rmsd_receptor.addCollectiveVariable('rmsd', rmsd_cv)
+#rmsd_receptor.addGlobalParameter('rmsd_target', rmsd_receptor_0)
+#rmsd_receptor.addGlobalParameter('k_rmsd', k_rmsd)
+#system.addForce(rmsd_receptor)
+# Set up integrator to integrate forces
+integrator = LangevinIntegrator(298*kelvin, 1/picosecond, TIME_STEP)
+
+# Pull together all the elements for the simulation
+simulation = Simulation(prmtop.topology, system, integrator, platform)
+simulation.context.setPositions(inpcrd.positions)
+if inpcrd.boxVectors is not None:
+    simulation.context.setPeriodicBoxVectors(*inpcrd.boxVectors)
+
+# Energy minimisation
+simulation.minimizeEnergy()
+
+# Setup reporters
+
+simulation.reporters.append(DCDReporter('TTSCP_L102C_DDSB_rep2_200ns.dcd', LOG_FREQUENCY_STEPS))
+simulation.reporters.append(StateDataReporter(
+        stdout,
+        LOG_FREQUENCY_STEPS,
+        step=True,
+        totalSteps=TOTAL_STEPS,
+        potentialEnergy=True,
+        temperature=True,
+        kineticEnergy=True,
+        speed=True,
+        volume=True,
+        progress=True
+    )
+)
+simulation.reporters.append(
+    StateDataReporter(
+        'TTSCP_L102C_DDSB_rep2_200ns.txt',
+        LOG_FREQUENCY_STEPS,
+        step=True,
+        totalSteps=TOTAL_STEPS,
+        potentialEnergy=True,
+        temperature=True,
+        kineticEnergy=True,
+        speed=True,
+        volume=True,
+        progress=True
+    )
+)
+simulation.reporters.append(CheckpointReporter('TTSCP_L102C_DDSB_rep2_200ns.chk', 5000))
+
+# Run the simulation!
+simulation.step(TOTAL_STEPS)
+
+
+# In[ ]:
+
+
+
+
